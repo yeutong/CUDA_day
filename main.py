21dfc9a96783fdca8c005b24c2de53b09f5cbce0
Yeu-Tong Lau <yeutong00@gmail.com>
2023-06-18 02:20:14 +0100

    compile multiple kernel files

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import subprocess
 from torch import Tensor
 from utils.kernel_func_decorator import kernel_function
 
-kernel_file = "add3_kernel.cu"
+kernel_files = "add3_kernel.cu add_kernel.cu"
 
 # Compile the CUDA kernel using nvcc
 # compiles the source code in kernel_file into a shared library named kernel.so that can be loaded and run by your Python program.
-nvcc_command = "nvcc -shared --compiler-options '-fPIC' -o kernel.so " + kernel_file + " -I /usr/include/torch/csrc/api/include/"
+nvcc_command = "nvcc -shared --compiler-options '-fPIC' -o kernel.so " + kernel_files + " -I /usr/include/torch/csrc/api/include/"
 result = subprocess.run(nvcc_command, shell=True, text=True, capture_output=True)
 
 if result.returncode != 0:
@@ -19,6 +19,7 @@ if result.returncode != 0:
     exit(1)
 else:
     print(f"compilation successful")
+
 
 # Initialize CUDA variables
 device = t.device("cuda")
@@ -30,13 +31,13 @@ b = t.ones(size, device=device)
 c = t.ones(size, device=device)
 d = t.zeros(size, device=device)
 
-# @kernel_function(lib_path=f"{os.getcwd()}/kernel.so")
-# def sum_cuda(lib, a: Tensor, b: Tensor, c: Tensor, size: int, stream: t.cuda.streams.Stream) -> None:
-#     return lib.add_cuda
 
-# sum_cuda(a, b, c, size)
-# print(c)
+@kernel_function(lib_path=f"{os.getcwd()}/kernel.so")
+def sum_cuda(lib, a: Tensor, b: Tensor, c: Tensor, size: int, stream: t.cuda.streams.Stream) -> None:
+    return lib.add_cuda
 
+sum_cuda(a, b, c, size)
+print(c)
 
 
 @kernel_function(lib_path=f"{os.getcwd()}/kernel.so")
